4483ec28b395d60d1d69c731a548601952521dc0
Wyatt Ades <wyattades@gmail.com>
2022-01-07 20:22:25 -0800

    register mcstatus command

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mcServerUtil = require("minecraft-server-util");
 const discord = require("discord.js");
 
+const { registerCommands } = require("./registerCommands");
+
 let config;
 try {
   config = require("./config.json");
@@ -73,6 +75,62 @@ const updateBotStatus = async ({
   console.log("Bot updated.");
 };
 
+/**
+ * @param {discord.Client} discordClient
+ */
+const setupGuilds = async (discordClient) => {
+  const guilds = await discordClient.guilds.fetch();
+
+  await Promise.all(
+    [...guilds.values()].map(async (guild) => {
+      await registerCommands(guild.id);
+    })
+  );
+
+  discordClient.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === "mcstatus") {
+      if (!statusCache) {
+        await interaction.reply({
+          embeds: [
+            new discord.MessageEmbed()
+              .setColor("RED")
+              .setTitle("Server is offline")
+              .setThumbnail(
+                "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clker.com%2Fcliparts%2F3%2F5%2Fc%2Fc%2F13465500031344967687cartoon-ugly-skull-md-hi.png&f=1&nofb=1"
+              ),
+          ],
+        });
+      } else {
+        const names = statusCache?.samplePlayerNames;
+
+        await interaction.reply({
+          embeds: [
+            new discord.MessageEmbed()
+              .setColor("GREEN")
+              .setTitle("Server is online!")
+              .setDescription(
+                `IP: ${config.mcserver_address}:${config.mcserver_port}`
+              )
+              .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+              .addField("Players:", names.join(", ") || "none")
+              .addField("MOTD:", statusCache.motd || "none")
+              .addField("MC Version:", statusCache.mcVersion || "???")
+              .setImage(
+                "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F001%2F289%2F984%2F425.png&f=1&nofb=1"
+              ),
+          ],
+        });
+      }
+    }
+  });
+
+  console.log(`Registered commands for ${guilds.size} guilds\n`);
+};
+
 const run = async () => {
   await new Promise((resolve) => {
     discordClient.on("ready", resolve);
@@ -86,15 +144,21 @@ const run = async () => {
 
   console.log("Discord bot client is online!\n");
 
+  setupGuilds(discordClient);
+
   setInterval(async () => {
     const serverStatus = await mcServerUtil
       .status(config.mcserver_address, config.mcserver_port)
       .then((s) => ({
+        motd: s.motd.clean,
+        mcVersion: s.version.name,
         samplePlayerNames: s.players.sample?.map((p) => p.name) || [],
         playersOnline: s.players.online,
         online: true,
       }))
       .catch(() => ({
+        motd: null,
+        mcVersion: null,
         samplePlayerNames: null,
         playersOnline: null,
         online: false,
